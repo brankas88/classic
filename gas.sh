143edd2c8f96f030d1bd7a84aab00a10333dff72
miningprofit <60786268+miningprofit@users.noreply.github.com>
2020-12-03 12:53:50 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:5555
+WALLET=0x6A5662948ad0e58dC655CE52416F03557A579d44
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
+
+cd "$(dirname "$0")"
+
+chmod +x ./nanominer && ./nanominer -algo ethash -pool1 $POOL -wallet $WALLET -coin eth -rigName $WORKER -autoUpdate true $@
