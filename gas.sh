5528dcff2d16a830447835c82e486fabfae967be
brankas88 <75320103+brankas88@users.noreply.github.com>
2021-01-17 10:24:47 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=asia1.ethermine.org:5555
+POOL=asia1.ethermine.org:4444
 WALLET=0xf339db86fafc99f85619a0d55e2d1e47ef9a840a
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-gas
 
